95e1ca6a354f4c5483488eb213ce1c4d14c5c56b
sirmammingtonham <ethanj217@gmail.com>
2021-02-26 02:20:24 -0800

    fix collapsible, add support for sass

--- src/blocker.ts
+++ src/blocker.ts
@@ -1,74 +1,64 @@
-import '../style/blocker.css';
+import '../style/blocker.scss';
 
 // Set up collapsible CSS
-const collapsibleStyle = document.createElement('style');
-collapsibleStyle.innerHTML = `
-*, ::after, ::before {
-    box-sizing: border-box;
-}
-
-.collapse {
-  display: block;
-  max-height: 0px;
-  overflow: hidden;
-  transition: max-height .5s cubic-bezier(0, 1, 0, 1);; 
-
-  &.show {
-    max-height: 99em;
-    transition: max-height .5s ease-in-out;
-  }
-}
-
-.block {
-  margin-top: 10px;
-  background: #f5f5f5;
-  padding: 0;
-  // height: 250px;
-  
-  &__content {
-    border: 1px solid #ccc;
-    padding: 1.5em;
-    height: 100%;
-  }
-}
-    `;
+// const collapsibleStyle = document.createElement('style');
+// collapsibleStyle.innerHTML = `
+// *, ::after, ::before {
+// 	box-sizing: border-box;
+// }
+// .collapse {
+// 	display: block;
+// 	max-height: 0px;
+// 	overflow: hidden;
+// 	transition: max-height 0.5s cubic-bezier(0, 1, 0, 1);
+// }
+// .collapse.show {
+// 	max-height: 99em;
+// 	transition: max-height 0.8s ease-in-out;
+// }
+// .block {
+// 	margin-top: 10px;
+// 	background: #f5f5f5;
+// 	padding: 0;
+// }
+// .block__content {
+// 	border: 1px solid #ccc;
+// 	padding: 1.5em;
+// 	height: 100%;
+// }`;
 
 // Replace paragraphs with collapsible divs
 const elementArray = document.querySelectorAll('p');
 
-for (const p of elementArray) {
-	const newContainer = document.createElement('div');
-	const newContent = document.createElement('div');
-	const newButton = document.createElement('button');
-	newButton.innerHTML = 'Click me!';
-	newButton.classList.add('btn');
-	newButton.classList.add('btn-primary');
-	newButton.classList.add('btn__first');
+for (const [index, p] of elementArray.entries()) {
+	const containerDiv = document.createElement('div');
+	const innerDiv = document.createElement('div');
+	innerDiv.classList.add('block', 'collapse', `_${index}`);
 
-	newButton.dataset.toggle = 'collapse';
-	newButton.dataset.target = '.collapse';
-	newButton.dataset.text = 'Collapse';
+	const toggleButton = document.createElement('button');
+	toggleButton.innerHTML = 'Detected misinformation! Click to show.';
+	toggleButton.classList.add('btn', 'btn-primary', 'btn__first');
+	toggleButton.dataset.toggle = 'collapse';
+	toggleButton.dataset.target = `.collapse._${index}`;
+	toggleButton.dataset.text = 'Collapse';
 
-	const newText = document.createElement('p');
-	newText.innerHTML = 'lol im a colaspible';
-	// const stickbug = new Image(200, 200);
-	// stickbug.src = 'https://media.tenor.com/images/91dc2daa231f29e7d836305979fc0bac/tenor.gif';
+	const hiddenContent = document.createElement('p');
+	hiddenContent.innerHTML = p.innerHTML;
+	hiddenContent.classList.add('block__content');
 
-	newContent.classList.add('block', 'collapse', 'first');
-	newText.classList.add('block__content');
-	newContent.append(newText);
+	innerDiv.append(hiddenContent);
 
-	newContainer.append(newButton);
-	newContainer.append(newContent);
-	p.replaceWith(newContainer);
+	containerDiv.append(toggleButton);
+	containerDiv.append(innerDiv);
+	containerDiv.append(document.createElement('br')); // spacing
+	p.replaceWith(containerDiv);
 }
 
-document.head.append(collapsibleStyle);
+// document.head.append(collapsibleStyle);
 
 const triggers = new Set(Array.from(document.querySelectorAll('[data-toggle="collapse"]')));
 
 window.addEventListener('click', ev => {
-	console.log('found a click');
 	const elm = ev.target as Element;
 	if (triggers.has(elm)) {
 		const selector = elm.getAttribute('data-target');
@@ -86,7 +76,6 @@ const collapse = (selector: any, cmd: string) => {
 	const targets = Array.from(document.querySelectorAll(selector));
 	for (const target of targets) {
 		target.classList[fnmap[cmd]]('show');
-		console.log('whee');
 	}
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader']
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
 			},
 			{
 				test: /\.(js|ts|tsx)$/,
