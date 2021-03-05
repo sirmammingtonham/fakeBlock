9259d908b07ed9182decc60cef616005d9d9d039
sirmammingtonham <ethanj217@gmail.com>
2021-03-05 01:45:30 -0800

    add button to toggle extension

--- src/blocker.ts
+++ src/blocker.ts
@@ -1,67 +1,70 @@
 import '../style/blocker.scss';
+import {browser} from 'webextension-polyfill-ts';
 import {pageType} from './util/page-type';
 
-chrome.storage.sync.get('enabled').then((enabled: any) => {
-	console.log(enabled);
+// check if the extension has been enabled by the user
+(async () => {
+	const retrieved = await browser.storage.local.get('enabled');
+	const enabled = !retrieved.enabled ?? true;
 	if (enabled) {
-		console.log(pageType(window.location.origin));
-
-		// Replace paragraphs with collapsible divs
-		const elementArray = document.querySelectorAll('p');
-
-		for (const [index, p] of Array.from(elementArray).entries()) {
-			const containerDiv = document.createElement('div');
-			const innerDiv = document.createElement('div');
-			innerDiv.classList.add('block', 'collapse', `_${index}`);
-
-			const toggleButton = document.createElement('button');
-			toggleButton.innerHTML = 'Detected misinformation! Click to show.';
-			toggleButton.classList.add('btn', 'btn-primary', 'btn__first');
-			toggleButton.dataset.toggle = 'collapse';
-			toggleButton.dataset.target = `.collapse._${index}`;
-			toggleButton.dataset.text = 'Collapse';
-
-			const hiddenContent = document.createElement('p');
-			hiddenContent.innerHTML = p.innerHTML;
-			hiddenContent.classList.add('block__content');
-
-			innerDiv.append(hiddenContent);
-
-			containerDiv.append(toggleButton);
-			containerDiv.append(innerDiv);
-			containerDiv.append(document.createElement('br')); // spacing
-			p.replaceWith(containerDiv);
-		}
-
-		// document.head.append(collapsibleStyle);
-
-		const triggers = new Set(Array.from(document.querySelectorAll('[data-toggle="collapse"]')));
-
-		window.addEventListener('click', ev => {
-			const elm = ev.target as Element;
-			if (triggers.has(elm)) {
-				const selector = elm.getAttribute('data-target');
-				collapse(selector, 'toggle');
-			}
-		}, false);
-
-		const fnmap: any = {
-			toggle: 'toggle',
-			show: 'add',
-			hide: 'remove'
-		};
-
-		const collapse = (selector: any, cmd: string) => {
-			const targets = Array.from(document.querySelectorAll(selector));
-			for (const target of targets) {
-				target.classList[fnmap[cmd]]('show');
-			}
-		};
-
-		console.log('blocker running in page!');
+		testBlocker();
 	}
-}
+})();
 
-).catch((error: any) => {
-	console.log(error);
-});
+function testBlocker() {
+	console.log(pageType(window.location.origin));
+
+	// Replace paragraphs with collapsible divs
+	const elementArray = document.querySelectorAll('p');
+
+	for (const [index, p] of Array.from(elementArray).entries()) {
+		const containerDiv = document.createElement('div');
+		const innerDiv = document.createElement('div');
+		innerDiv.classList.add('block', 'collapse', `_${index as number}`);
+
+		const toggleButton = document.createElement('button');
+		toggleButton.innerHTML = 'Detected misinformation! Click to show.';
+		toggleButton.classList.add('btn', 'btn-primary', 'btn__first');
+		toggleButton.dataset.toggle = 'collapse';
+		toggleButton.dataset.target = `.collapse._${index as number}`;
+		toggleButton.dataset.text = 'Collapse';
+
+		const hiddenContent = document.createElement('p');
+		hiddenContent.innerHTML = p.innerHTML;
+		hiddenContent.classList.add('block__content');
+
+		innerDiv.append(hiddenContent);
+
+		containerDiv.append(toggleButton);
+		containerDiv.append(innerDiv);
+		containerDiv.append(document.createElement('br')); // spacing
+		p.replaceWith(containerDiv);
+	}
+
+	// document.head.append(collapsibleStyle);
+
+	const triggers = new Set(Array.from(document.querySelectorAll('[data-toggle="collapse"]')));
+
+	window.addEventListener('click', ev => {
+		const elm = ev.target as Element;
+		if (triggers.has(elm)) {
+			const selector = elm.getAttribute('data-target');
+			collapse(selector, 'toggle');
+		}
+	}, false);
+
+	const fnmap: any = {
+		toggle: 'toggle',
+		show: 'add',
+		hide: 'remove'
+	};
+
+	const collapse = (selector: any, cmd: string) => {
+		const targets = Array.from(document.querySelectorAll(selector));
+		for (const target of targets) {
+			target.classList[fnmap[cmd]]('show');
+		}
+	};
+
+	console.log('blocker running in page!');
+}
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,16 +1,26 @@
+import {browser} from 'webextension-polyfill-ts';
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 export default class App extends Component {
+	// enabled: boolean;
+	// constructor(props: any) {
+	// 	super(props);
+	// }
 	async buttonclick() {
-		const enabled = await chrome.storage.sync.get('enabled');
-		chrome.storage.sync.set({
-			enabled
-		});
+		const retrieved = await browser.storage.local.get('enabled');
+		const enabled = !retrieved.enabled ?? true; // check if anything is assigned
+
+		(async () => {
+			await browser.storage.local.set({
+				enabled
+			});
+			await browser.tabs.reload();
+		})();
 	}
 
 	render() {
-		return <button onClick={this.buttonclick}>Click me!</button>;
+		return <button onClick={this.buttonclick}>Toggle fakeBlock!</button>;
 	}
 }
 const appContainer = document.querySelector('#app');
