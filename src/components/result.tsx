773f3c57dee708f6aa046dd9b51ebcf67c19791b
Julian L <jthemage101@gmail.com>
2021-03-22 00:39:24 -0400

    result page mocked up

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,30 +1,27 @@
 // import {browser} from 'webextension-polyfill-ts';
 import React from 'react';
 import {render} from 'react-dom';
-
+import {Chip, Container, Box} from '@material-ui/core';
+import {Warning, Announcement, SentimentVeryDissatisfied} from '@material-ui/icons';
 export default class Result extends React.Component {
-	state = {
-		count: 0
-	};
-
-	increment = () => {
-		this.setState({
-			count: (this.state.count + 1)
-		});
-	};
-
-	decrement = () => {
-		this.setState({
-			count: (this.state.count - 1)
-		});
-	};
-
 	render() {
 		return (
 			<div>
-				<h1>{this.state.count}</h1>
-				<button onClick={this.increment}>Increment</button>
-				<button onClick={this.decrement}>Decrement</button>
+				<Container maxWidth="sm">
+					<h1>The text was blocked for the following reasons.</h1>
+				</Container>
+				<Container maxWidth="sm">
+					<Chip color="secondary" label="Fake News" icon={<Announcement />} />
+					<Chip color="secondary" label="Deepfaked" icon={<Warning />} />
+					<Chip color="primary" label="Based" icon={<SentimentVeryDissatisfied />} />
+				</Container>
+				<Container maxWidth="sm">
+					<h1>Here are some stats you may be interested in:</h1>
+					<Box>
+						<p>Wow stat goes here</p>
+						<p>Another one</p>
+					</Box>
+				</Container>
 			</div>
 		);
 	}
