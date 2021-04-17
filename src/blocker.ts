3162d081d52012bb0ee8309aa6e6161f9a322a44
deanvogel <dnv185@hotmail.com>
2021-04-16 22:54:30 -0400

    still need to fix synchronization of blocker

--- src/blocker.ts
+++ src/blocker.ts
@@ -32,6 +32,7 @@ function createCollapsible(p: Element, index: number, result: ClassifierOutput) 
 	toggleButton.dataset.toggle = 'collapse';
 	toggleButton.dataset.target = `.collapse._${index}`;
 	toggleButton.dataset.text = 'Collapse';
+	toggleButton.id = `#fakeindex_${index}`;
 
 	const hiddenContent = document.createElement('p');
 	hiddenContent.innerHTML = p.innerHTML;
@@ -58,12 +59,14 @@ function createCollapsible(p: Element, index: number, result: ClassifierOutput) 
 // waits until the page is done loading and then collects all of the wanted elements
 // on the page that containt text and sends it through to the machine learning model
 // If the result of the ml model flags the text, then the element is wrapped in a collapsible and is 'blocked'
-async function runBlocker() {
+function runBlocker() {
 	// checkLinks();
 	// Replace paragraphs with collapsible divs
 	// Add support later for other tags
 	const elementArray = document.querySelectorAll('p,h1,h2,h3,h4,h5,h6,dd,li,text');
-	await Promise.all([...elementArray].map(async (p, index) => {
+	console.log("elementArray length: ", elementArray.length);
+	console.time("elementarray time");
+	[...elementArray].map((p, index) => {
 		if (!p.textContent || p.textContent.split(' ').length < 15) {
 			return; // skip scanning content that doesn't look to be a complete sentence (< 15 words)
 		}
@@ -72,15 +75,29 @@ async function runBlocker() {
 			// should maybe have it so if aggregate is mixed, text is highlighted but not blocked
 			if (result && result.valueAggregate !== AggregateLabels.reliable) {
 				createCollapsible(p, index, result);
+				console.log("piece of content blocked");
+				const ttt = document.getElementById(`#fakeindex_${index}`);
+				if(ttt)
+				{
+					ttt.addEventListener('click', ev => {
+					const elm = ev.target as Element;
+					const selector = elm.getAttribute('data-target');
+					collapse(selector, 'toggle');
+					console.log("we got the button on it");
+					}, false);
+				}
 			}
 		});
-	}));
+	});
+	console.timeEnd("elementarray time");
 
 	const divArray = document.querySelectorAll('span'); // div
+	console.log("divArray length: ", divArray.length);
 	// go through divs, try getting only divs with text in them
 	// general solution, block all divs that have no children, but this needs to be worked out
 	// text can have <b>(bold), <i>(italic) elements and things like that which prevent the blocking
-	await Promise.all([...divArray].map(async (p, index) => {
+	console.time("divarray time");
+	[...divArray].map((p, index) => {
 		if (!p.textContent || p.textContent.split(' ').length < 15) { // || p.hasChildNodes
 			return; // skip scanning content that doesn't look to be a complete sentence (< 15 words)
 		}
@@ -88,20 +105,21 @@ async function runBlocker() {
 		return browser.runtime.sendMessage({message: 'scanText', text: p.textContent}).then((result: ClassifierOutput) => {
 			if (result && result.valueAggregate !== AggregateLabels.reliable) {
 				createCollapsible(p, index, result);
+				console.log("piece of content blocked");
+				const ttt = document.getElementById(`#fakeindex_${index}`);
+				if(ttt)
+				{
+					ttt.addEventListener('click', ev => {
+					const elm = ev.target as Element;
+					const selector = elm.getAttribute('data-target');
+					collapse(selector, 'toggle');
+					console.log("we got the button on it");
+					}, false);
+				}
 			}
 		});
-	}));
-
-	const triggers = new Set([...document.querySelectorAll('[data-toggle="collapse"]')]);
-
-	window.addEventListener('click', ev => {
-		const elm = ev.target as Element;
-		if (triggers.has(elm)) {
-			const selector = elm.getAttribute('data-target');
-			collapse(selector, 'toggle');
-		}
-	}, false);
-
+	});
+	console.timeEnd("divarray time");
 	const fnmap: any = {
 		toggle: 'toggle',
 		show: 'add',
