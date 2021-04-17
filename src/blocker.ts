9cbfb82d39735d52c11a5f5603ae0044219003f8
sirmammingtonham <ethanj217@gmail.com>
2021-04-16 23:30:05 -0700

    revert blocker for now, until we can fix sync

--- src/blocker.ts
+++ src/blocker.ts
@@ -2,7 +2,6 @@ import '../style/blocker.scss';
 import {browser} from 'webextension-polyfill-ts';
 import {Websites, pageType} from './util/page-type';
 import {ClassifierOutput, AggregateLabels} from './detection/classifier';
-// import {AnyInputs} from '@tensorflow/tfjs-core';
 
 // check if the extension has been enabled by the user
 (async () => {
@@ -35,7 +34,6 @@ function createCollapsible(p: Element, index: number, result: ClassifierOutput) 
 	toggleButton.dataset.toggle = 'collapse';
 	toggleButton.dataset.target = `.collapse._${index}`;
 	toggleButton.dataset.text = 'Collapse';
-	toggleButton.id = `#fakeindex_${index}`;
 
 	const hiddenContent = document.createElement('p');
 	hiddenContent.innerHTML = p.innerHTML;
@@ -67,102 +65,49 @@ async function runBlocker() {
 	// Replace paragraphs with collapsible divs
 	// Add support later for other tags
 	let count = 0;
+
 	const elementArray = document.querySelectorAll('p,h1,h2,h3,h4,h5,h6,dd,li,text');
-	console.log('elementArray length:', elementArray.length);
-	console.time('elementarray time');
-	await Promise.all([...elementArray].map<Promise<any>>(async (p, index) => {
+	await Promise.all([...elementArray].map(async (p, index) => {
 		if (!p.textContent || p.textContent.split(' ').length < 15) {
 			return; // skip scanning content that doesn't look to be a complete sentence (< 15 words)
 		}
 
-		// return browser.runtime.sendMessage({message: 'scanText', text: p.textContent}).then(result => {
-		// 	// should maybe have it so if aggregate is mixed, text is highlighted but not blocked
-		// 	if (result && result.valueAggregate !== AggregateLabels.reliable) {
-		// 		createCollapsible(p, index, result);
-		// 		count += 1;
-		// 		console.log('piece of content blocked');
-		// 		const ttt = document.querySelector(`#fakeindex_${index}`);
-		// 		if (ttt) {
-		// 			ttt.addEventListener('click', ev => {
-		// 				const elm = ev.target as Element;
-		// 				const selector = elm.getAttribute('data-target');
-		// 				collapse(selector, 'toggle');
-		// 				console.log('we got the button on it');
-		// 			}, false);
-		// 		}
-		// 	}
-		// });
-
-		const result = await browser.runtime.sendMessage({message: 'scanText', text: p.textContent});
-		// should maybe have it so if aggregate is mixed, text is highlighted but not blocked
-		if (result && result.valueAggregate !== AggregateLabels.reliable) {
-			createCollapsible(p, index, result);
-			count += 1;
-			console.log(`count is now ${count}`);
-			console.log('piece of content blocked');
-			const ttt = document.querySelector(`#fakeindex_${index}`);
-			if (ttt) {
-				ttt.addEventListener('click', ev => {
-					const elm = ev.target as Element;
-					const selector = elm.getAttribute('data-target');
-					collapse(selector, 'toggle');
-					console.log('we got the button on it');
-				}, false);
+		return browser.runtime.sendMessage({message: 'scanText', text: p.textContent}).then(result => {
+			// should maybe have it so if aggregate is mixed, text is highlighted but not blocked
+			if (result && result.valueAggregate !== AggregateLabels.reliable) {
+				count += 1;
+				createCollapsible(p, index, result);
 			}
-		}
-
-		return result;
+		});
 	}));
-	console.timeEnd('elementarray time');
 
 	const divArray = document.querySelectorAll('span'); // div
-	console.log('divArray length:', divArray.length);
 	// go through divs, try getting only divs with text in them
 	// general solution, block all divs that have no children, but this needs to be worked out
 	// text can have <b>(bold), <i>(italic) elements and things like that which prevent the blocking
-	console.time('divarray time');
 	await Promise.all([...divArray].map(async (p, index) => {
 		if (!p.textContent || p.textContent.split(' ').length < 15) { // || p.hasChildNodes
 			return; // skip scanning content that doesn't look to be a complete sentence (< 15 words)
 		}
 
-		// return browser.runtime.sendMessage({message: 'scanText', text: p.textContent}).then((result: ClassifierOutput) => {
-		// 	if (result && result.valueAggregate !== AggregateLabels.reliable) {
-		// 		createCollapsible(p, index, result);
-		// 		count += 1;
-		// 		console.log('piece of content blocked');
-		// 		const ttt = document.querySelector(`#fakeindex_${index}`);
-		// 		if (ttt) {
-		// 			ttt.addEventListener('click', ev => {
-		// 				const elm = ev.target as Element;
-		// 				const selector = elm.getAttribute('data-target');
-		// 				collapse(selector, 'toggle');
-		// 				console.log('we got the button on it');
-		// 			}, false);
-		// 		}
-		// 	}
-		// });
-
-		const result = await browser.runtime.sendMessage({message: 'scanText', text: p.textContent});
-		if (result && result.valueAggregate !== AggregateLabels.reliable) {
-			createCollapsible(p, index, result);
-			console.log(`count is now ${count}`);
-			count += 1;
-			console.log('piece of content blocked');
-			const ttt = document.querySelector(`#fakeindex_${index}`);
-			if (ttt) {
-				ttt.addEventListener('click', ev => {
-					const elm = ev.target as Element;
-					const selector = elm.getAttribute('data-target');
-					collapse(selector, 'toggle');
-					console.log('we got the button on it');
-				}, false);
+		return browser.runtime.sendMessage({message: 'scanText', text: p.textContent}).then((result: ClassifierOutput) => {
+			if (result && result.valueAggregate !== AggregateLabels.reliable) {
+				count += 1;
+				createCollapsible(p, index, result);
 			}
-		}
-
-		return result;
+		});
 	}));
-	console.timeEnd('divarray time');
+
+	const triggers = new Set([...document.querySelectorAll('[data-toggle="collapse"]')]);
+
+	window.addEventListener('click', ev => {
+		const elm = ev.target as Element;
+		if (triggers.has(elm)) {
+			const selector = elm.getAttribute('data-target');
+			collapse(selector, 'toggle');
+		}
+	}, false);
+
 	const fnmap: any = {
 		toggle: 'toggle',
 		show: 'add',
@@ -176,16 +121,7 @@ async function runBlocker() {
 		}
 	};
 
-	// browser.runtime.onMessage.addListener(
-	// 	async (message: any) => {
-	// 		switch (message.type) {
-	// 			case 'getCount':
-	// 				return Promise.resolve(count);
-	// 			default:
-	// 				return Promise.resolve('problem with counting collapsible');
-	// 		}
-	// 	}
-	// );
 	console.log('blocker running in page!');
+
 	return count;
 }
